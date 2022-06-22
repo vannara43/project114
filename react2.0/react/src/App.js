4f414b1b45db3d47388ebea484b740c5491df765
vannara43 <36809428+vannara43@users.noreply.github.com>
2022-06-22 14:25:13 -0700

    another commit

---
+++ react2.0/react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Default Page</h1>
+      <p>This is a paragrapg</p>
+      <p>Added by Vann T</p>
+    </React.Fragment>
+  );
+}
+
+export default App;
