631228e0d6ffc75387f900fba15d788d22886ff9
vannara43 <36809428+vannara43@users.noreply.github.com>
2022-06-28 15:44:44 -0700

    test table add request

---
+++ dotnet/Sabio.Web.Api/Controllers/TestTableOneAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sabio.Web.Api.Controllers
+{
+    [Route("api/testtableone")]
+    [ApiController]
+    public class TestTableOneAPIController : ControllerBase
+    {
+    }
+}
