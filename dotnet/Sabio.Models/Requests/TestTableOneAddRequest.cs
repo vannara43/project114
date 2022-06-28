9fc7cd2095981b5cb03a45a93e67622ec8dd639a
vannara43 <36809428+vannara43@users.noreply.github.com>
2022-06-28 15:47:40 -0700

    test table add request

--- dotnet/Sabio.Models/Requests/TestTableOneAddRequest.cs
+++ dotnet/Sabio.Models/Requests/TestTableOneAddRequest.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Sabio.Models.Requests
 {
-    internal class TestTableOneAddRequest
+    public class TestTableOneAddRequest
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SKU { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+        public string ImgUrl { get; set; }
     }
 }
