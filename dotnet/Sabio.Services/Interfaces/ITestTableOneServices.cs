583e8fc6e78b6c38af79dd0ac08591e85a0f7408
JaredWilliams562 <jaredwilliams562@gmail.com>
2022-07-13 16:36:29 -0700

    get all api

--- dotnet/Sabio.Services/Interfaces/ITestTableOneServices.cs
+++ dotnet/Sabio.Services/Interfaces/ITestTableOneServices.cs
@@ -1,9 +1,12 @@
-﻿using Sabio.Models.Requests;
+﻿using Sabio.Models;
+using Sabio.Models.Domain;
+using Sabio.Models.Requests;
 
 namespace Sabio.Services
 {
     public interface ITestTableOneServices
     {
         int Add(TestTableOneAddRequest model);
+        Paged<TestTableOne> GetAll(int pageIndex, int pageSize);
     }
 }
