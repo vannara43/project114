3c047cefbaf0ab11408ad2d633b0a222c0bfaa92
vannara43 <36809428+vannara43@users.noreply.github.com>
2022-08-01 16:36:03 -0700

    fixed image size for developer team

--- react/src/App.js
+++ react/src/App.js
@@ -27,14 +27,14 @@ function App() {
       <div className="container">
         <h1>Project 114</h1>
         <hr />
-        <h3>Meet our developers</h3>
+        <h3 className="my-3">Meet our developers</h3>
         <div className="row ">
           <div className="col col-2">
-            <img className="rounded-circle" alt="" src="https://media-exp1.licdn.com/dms/image/C5603AQGscng7POt26g/profile-displayphoto-shrink_200_200/0/1648413781916?e=1665014400&v=beta&t=nZPZb-vSH5qJZB16q9ufTSlA9xHLN85_Y_XWn_1EV3o" />
+            <img height="100px" width="100px" className="rounded-circle" alt="" src="https://media-exp1.licdn.com/dms/image/C5603AQGscng7POt26g/profile-displayphoto-shrink_200_200/0/1648413781916?e=1665014400&v=beta&t=nZPZb-vSH5qJZB16q9ufTSlA9xHLN85_Y_XWn_1EV3o" />
             <p className="">Jared Williams</p>
           </div>
           <div className="col">
-            <img className="rounded-circle" alt="" src="https://media-exp1.licdn.com/dms/image/C5603AQEcL8kmvPemtQ/profile-displayphoto-shrink_200_200/0/1652878926495?e=1665014400&v=beta&t=zqeXGf73gG9nkbe1y8cM26kcV6brfzJ57oHiYjfRjD0" />
+            <img height="100px" width="100px" className="rounded-circle" alt="" src="https://media-exp1.licdn.com/dms/image/C5603AQEcL8kmvPemtQ/profile-displayphoto-shrink_200_200/0/1652878926495?e=1665014400&v=beta&t=zqeXGf73gG9nkbe1y8cM26kcV6brfzJ57oHiYjfRjD0" />
             <p>Vannara Thong</p>
           </div>
         </div>
