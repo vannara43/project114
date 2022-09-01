7003760efe58f8281709b5e3895a75dacc41e8ed
vannara43 <36809428+vannara43@users.noreply.github.com>
2022-09-01 01:29:55 -0700

    added products

---
+++ dotnet/Sabio.Models/Domain/Products.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SKU { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+        public string ImgUrl { get; set; }
+
+    }
+}
