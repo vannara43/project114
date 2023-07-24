87d8b5e4d4fe32d5587b8848508455c339416d47
Vannara Thong <vannarathong@gmail.com>
2023-07-24 10:41:48 -0700

    updated home and profile

--- react/src/components/landing/home.jsx
+++ react/src/components/landing/home.jsx
@@ -7,7 +7,15 @@ function Home() {
   return (
     //insert HTML Code below
     <React.Fragment>
+      <div className="container border bg-dark">
       <h2 className="">Home Page</h2>
+        <div style={{"width":"auto","height":"1000px","padding":"45%","margin":"30px"}} className="border bg-secondary">
+          <h1>MAIN</h1>
+        </div>
+        <div style={{"width":"auto","height":"200px","padding-left":"45%", "margin":"30px"}} className="border bg-secondary">
+          <h1>Contact</h1>
+        </div>
+      </div>
     </React.Fragment>
     //insert HTML Code above
   );
